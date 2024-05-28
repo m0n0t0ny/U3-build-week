28d0171fd12d2db0b271840164fb7ccae529113a
Antonio Bertuccio <46935714+m0n0t0ny@users.noreply.github.com>
2024-05-28 21:45:53 +0200

    feat: Add apiUrl and mockUrl to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://657c1dde394ca9e4af157886.mockapi.io/instagrang',
+  mockUrl: 'https://657c1dde394ca9e4af157886.mockapi.io/instagrang',
 };
